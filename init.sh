3f980b96e7c30160b37da3a2fd203e72be1f3e50
Eric D. Schabell <eric@schabell.org>
2018-01-10 13:06:15 +0100

    Updated project to bpmsuite 6.4, eap 7.0 and datavirt 6.3.

--- init.sh
+++ init.sh
@@ -4,24 +4,23 @@ AUTHORS="Niraj Patel, Shepherd Chengeta, Van Halbert,"
 AUTHORS2="Andrew Block, Eric D. Schabell"
 PROJECT="git@github.com:eschabell/bpms-dv-travel-agency-integration-demo.git"
 PRODUCT="JBoss BPM Suite"
-JBOSS_HOME=./target/jboss-bpmsuite-6.2
+JBOSS_HOME=./target/jboss-eap-7.0
 SERVER_DIR=$JBOSS_HOME/standalone/deployments/
 SERVER_CONF=$JBOSS_HOME/standalone/configuration/
 SERVER_BIN=$JBOSS_HOME/bin
 SRC_DIR=./installs
 SUPPORT_DIR=./support
 PRJ_DIR=./projects
-BPMS=jboss-bpmsuite-installer-6.2.0.BZ-1299002.jar
-EAP=jboss-eap-6.4.0-installer.jar
-EAP_PATCH=jboss-eap-6.4.4-patch.zip
-DV=jboss-dv-installer-6.2.0.redhat-3.jar
+BPMS=jboss-bpmsuite-6.4.0.GA-deployable-eap7.x.zip
+EAP=jboss-eap-7.0.0-installer.jar
+DV=jboss-dv-6.3.0-1-installer.jar
 DV_PRODUCT="JBoss DataVirt"
-DV_JBOSS_HOME=./target/jboss-dv-6.2
+DV_JBOSS_HOME=./target/jboss-dv-6.3
 DV_SERVER_DIR=$DV_JBOSS_HOME/standalone/deployments/
 DV_SERVER_CONF=$DV_JBOSS_HOME/standalone/configuration/
 DV_SERVER_BIN=$DV_JBOSS_HOME/bin
-DV_VERSION=6.2
-VERSION=6.2
+DV_VERSION=6.3
+VERSION=6.4
 
 
 # wipe screen.
@@ -56,41 +55,31 @@ command -v mvn -q >/dev/null 2>&1 || { echo >&2 "Maven is required but not insta
 
 # make some checks first before proceeding.	
 if [ -r $SRC_DIR/$EAP ] || [ -L $SRC_DIR/$EAP ]; then
-	echo EAP Product sources are present...
+	echo "EAP Product sources are present..."
 	echo
 else
-	echo Need to download $EAP package from the Customer Portal 
-	echo and place it in the $SRC_DIR directory to proceed...
-	echo
-	exit
-fi
-
-if [ -r $SRC_DIR/$EAP_PATCH ] || [ -L $SRC_DIR/$EAP_PATCH ]; then
-	echo EAP patches are present...
-	echo
-else
-	echo Need to download $EAP_PATCH package from the Customer Portal 
-	echo and place it in the $SRC_DIR directory to proceed...
+	echo "Need to download $EAP package from from https://developers.redhat.com/products/eap/download"
+	echo "and place it in the $SRC_DIR directory to proceed..."
 	echo
 	exit
 fi
 
 if [ -r $SRC_DIR/$BPMS ] || [ -L $SRC_DIR/$BPMS ]; then
-		echo BPMS Suite sources are present...
+		echo "BPM Suite sources are present..."
 		echo
 else
-		echo Need to download $BPMS package from the Customer Portal 
-		echo and place it in the $SRC_DIR directory to proceed...
+		echo "Need to download $BPMS package from from https://developers.redhat.com/products/bpmsuite/download"
+		echo "and place it in the $SRC_DIR directory to proceed..."
 		echo
 		exit
 fi
 
 if [ -r $SRC_DIR/$DV ] || [ -L $SRC_DIR/$DV ]; then
-	echo DV product sources, present...
+	echo "DV product sources, present..."
 	echo
 else
-	echo Need to download $DV package from the Customer Portal 
-	echo and place it in the $SRC_DIR directory to proceed...
+	echo "Need to download $DV package from https://developers.redhat.com/products/datavirt/download"
+	echo "and place it in the $SRC_DIR directory to proceed..."
 	echo
 	exit
 fi
@@ -109,50 +98,17 @@ java -jar $SRC_DIR/$EAP $SUPPORT_DIR/installation-eap -variablefile $SUPPORT_DIR
 
 if [ $? -ne 0 ]; then
 	echo
-	echo Error occurred during JBoss EAP installation!
+	echo "Error occurred during JBoss EAP installation!"
 	exit
 fi
 
 echo
-echo "Applying JBoss EAP 6.4.4 patch now..."
+echo "Installing JBoss BPM Suite now..."
 echo
-$JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SRC_DIR/$EAP_PATCH"
+unzip -qo $SRC_DIR/$BPMS -d target
 
 if [ $? -ne 0 ]; then
-	echo
-	echo Error occurred during JBoss EAP patching!
-	exit
-fi
-
-echo
-echo "JBoss BPM Suite installer running now..."
-echo
-java -jar $SRC_DIR/$BPMS $SUPPORT_DIR/installation-bpms -variablefile $SUPPORT_DIR/installation-bpms.variables
-
-if [ $? -ne 0 ]; then
-	echo Error occurred during $PRODUCT installation!
-	exit
-fi
-
-echo
-echo "JBoss EAP for DataVirt installer running now..."
-echo
-java -jar $SRC_DIR/$EAP $SUPPORT_DIR/installation-dv-eap -variablefile $SUPPORT_DIR/installation-dv-eap.variables
-
-if [ $? -ne 0 ]; then
-	echo
-	echo Error occurred during JBoss EAP for DataVirt installation!
-	exit
-fi
-
-echo
-echo "Applying JBoss EAP for DataVirt 6.4.4 patch now..."
-echo
-$DV_JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SRC_DIR/$EAP_PATCH"
-
-if [ $? -ne 0 ]; then
-	echo
-	echo Error occurred during JBoss EAP for DataVirt patching!
+	echo "Error occurred during $PRODUCT installation!"
 	exit
 fi
 
@@ -162,14 +118,19 @@ echo
 java -jar $SRC_DIR/$DV $SUPPORT_DIR/installation-dv -variablefile $SUPPORT_DIR/installation-dv.variables
 
 if [ $? -ne 0 ]; then
-	echo Error occurred during DV installation!
+	echo "Error occurred during DV installation!"
 	exit
 fi
 
 echo
 echo "  - enabling demo accounts role setup in application-roles.properties file..."
 echo
-cp $SUPPORT_DIR/application-roles.properties $SERVER_CONF
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u erics -p bpmsuite1! -ro analyst,admin,user,manager,taskuser,reviewerrole,employeebookingrole,kie-server,rest-all --silent
+
+if [ $? -ne 0 ]; then
+	echo "Error occurred during account role setup!"
+	exit
+fi
 
 echo "  - setting up demo projects..."
 echo
@@ -201,12 +162,6 @@ cp $SUPPORT_DIR/userinfo.properties $SERVER_DIR/business-central.war/WEB-INF/cla
 echo "  - making sure standalone.sh for server is executable..."
 echo
 chmod u+x $JBOSS_HOME/bin/standalone.sh
-
-# Optional: uncomment this to install mock data for BPM Suite.
-#
-#echo - setting up mock bpm dashboard data...
-#cp $SUPPORT_DIR/1000_jbpm_demo_h2.sql $SERVER_DIR/dashbuilder.war/WEB-INF/etc/sql
-#echo
 
 echo "  - making sure standalone.sh for DV server is executable..."
 echo
@@ -240,7 +195,7 @@ echo "=   $DV_SERVER_BIN/standalone.sh -Djboss.socket.binding.port-offset=100  =
 echo "=                                                                                   ="
 echo "=  After the previous server finishes starting, start the $PRODUCT with:     ="
 echo "=                                                                                   ="
-echo "=   $SERVER_BIN/standalone.sh                                   ="
+echo "=   $SERVER_BIN/standalone.sh                                        ="
 echo "=                                                                                   ="
 echo "=  Login into business central at:                                                  ="
 echo "=                                                                                   ="
